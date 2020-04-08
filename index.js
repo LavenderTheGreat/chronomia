3865f05d0b3061e2f724d9f12c8919326d1ddb3b
LavenderTheGreat <dach.soa@gmail.com>
2020-04-08 15:11:05 +0100

    Double breaks for breathing space

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function _updatePreview(file){
 
 	for (var i = 0; i < songFile.bpmChanges.length; i++) {
 		if (songFile.bpmChanges[i].bpm < 0) {
-			document.getElementById('currentWarnings').innerHTML = document.getElementById('currentWarnings').innerHTML + '<b>Negative BPMS will cause errors!</b> Negative BPM: Beat #' + songFile.bpmChanges[i].beat + " (" + songFile.bpmChanges[i].absolute + "s) - " + songFile.bpmChanges[i].bpm + "bpm" + '<br>'
+			document.getElementById('currentWarnings').innerHTML = document.getElementById('currentWarnings').innerHTML + '<b>Negative BPMS will cause errors!</b> Negative BPM: Beat #' + songFile.bpmChanges[i].beat + " (" + songFile.bpmChanges[i].absolute + "s) - " + songFile.bpmChanges[i].bpm + "bpm" + '<br><br>'
 		}
 		BPMOutput = BPMOutput + 'Beat #' + songFile.bpmChanges[i].beat + " (" + songFile.bpmChanges[i].absolute + "s) - " + songFile.bpmChanges[i].bpm + "bpm" + '<br>'
 	}
